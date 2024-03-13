fbe7a26f5b1ca7a81921fdfc0993e4e439698c3d
cath <tsailingyu@pm.me>
2024-03-13 13:49:15 -0400

    Created project with readme

---
+++ Boutiques/BoutiquesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoutiquesApp.swift
+//  Boutiques
+//
+//  Created by adobada on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct BoutiquesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
