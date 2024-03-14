c6e8e2245f51aa3d81b07b6c0fe5b1a418232e2a
cath <tsailingyu@pm.me>
2024-03-14 11:33:01 -0400

    Added and updated access token, added US base URL, and created NetworkManager

---
+++ Boutiques/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Boutiques
+//
+//  Created by adobada on 3/14/24.
+//
+
+import Foundation
+
+struct Constants {
+    enum AirtableAPI {
+        static let unitedStatesBaseURL = "https://api.airtable.com/v0/app3oZpEf8bSTrgCM/United%20States"
+    }
+}
---
+++ Boutiques/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  Boutiques
+//
+//  Created by adobada on 3/14/24.
+//
+
+import Foundation
+
+final class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    func fetchUnitedStatesBoutiques() async throws -> [Boutiques]
+}
