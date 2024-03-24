17f510303a7029d4ce069db774f243360687d34a
cath <tsailingyu@pm.me>
2024-03-24 12:06:50 -0400

    Removed logo from list view and added city

--- Boutiques/CustomViews/BoutiqueRowView.swift
+++ Boutiques/CustomViews/BoutiqueRowView.swift
@@ -13,28 +13,12 @@ struct BoutiqueRowView: View {
     
     var body: some View {
         HStack {
-            AsyncImage(url: URL(string: boutique.fields.logo?.last?.url ?? "No logo available")) { phase in
-                switch phase {
-                case .empty:
-                    Text("no available logo")
-                case .success(let image):
-                    image
-                        .resizable()
-                        .frame(width: 100, height: 100)
-                        .scaledToFit()
-                case .failure:
-                    Text("unable to load")
-                @unknown default:
-                    EmptyView()
-                }
-            }
-            Spacer()
             Text(boutique.fields.name)
                 .font(.custom(BaskervilleFont.regular, size: 20))
                 .kerning(2.0)
                 .foregroundStyle(.accent)
             Spacer()
-            Text(boutique.fields.state)
+            Text("\(boutique.fields.city), \(boutique.fields.state)")
                 .font(.custom(BaskervilleFont.regular, size: 20))
                 .foregroundStyle(.accent)
         }
